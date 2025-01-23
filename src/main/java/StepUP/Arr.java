dab7feea159db76f14dcb70abc4b36631afb7054
Art Tiz <tizner@mail.ru>
2025-01-23 13:01:26 +0700

    arr3

--- src/main/java/StepUP/Arr.java
+++ src/main/java/StepUP/Arr.java
@@ -5,20 +5,46 @@ import static java.lang.Math.abs;
 public class Arr {
     public static void main(String[] args) {
 
-        int[] arr = {1, -2, -7, 4, 2, 2, 5};
-        System.out.println(maxAbs(arr));
+        int[] arr = {1,-2,-7,4,-7,-2,1};
+        System.out.println(palindrom(arr));
 
     }
 
     ;
 
+    public static boolean palindrom(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arr[arr.length-1 - i])
+                return false;
+        }
+        return true;
+    }
+
+    ;
+
+
+    public static int countPositive(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0)
+                count++;
+        }
+        return count;
+    }
+
+    ;
+
+
     public static int maxAbs(int[] arr) {
         int max = 0;
+        int y = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (abs(arr[i]) > max)
-                max = arr[i];
+            if (abs(arr[i]) > max) {
+                max = abs(arr[i]);
+                y = i;
+            }
         }
-        return max;
+        return arr[y];
     }
 
     ;
