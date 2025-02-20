fd6ddf9a8a53f9d9abef97d6ece049d757d943d7
Bear <tizner@mail.ru>
2025-02-21 00:25:47 +0700

    classes

---
+++ src/main/java/StepUP/Line.java
@@ -0,0 +1,40 @@
+package StepUP;
+
+public class Line {
+
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        Tochka t1;
+        Tochka t2;
+
+    Line(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public Line(Tochka t1, Tochka t2) {
+        this.x1 = t1.x;
+        this.y1 = t1.y;
+        this.x2 = t2.x;
+        this.y2 = t2.y;
+
+    }
+    public float  getLength(){
+        return
+
+    }
+
+    public String toString() {
+        return "Линия от{" + x1 + ";" + y1 + "} до {" + x2+ ";" + y2 +"}";
+
+    }
+
+
+
+}
+
+
--- src/main/java/StepUP/StartClasses.java
+++ src/main/java/StepUP/StartClasses.java
@@ -3,15 +3,17 @@ package StepUP;
 public class StartClasses {
     public static void main(String[] args) {
         Tochka t1 = new Tochka(1,3);
-        Tochka t2 = new Tochka(1,3);
-        Tochka t3 = new Tochka(5,8);
+        Tochka t2 = new Tochka(4,7);
+
+        Line l1 = new Line(1,1,2,2);
+        Line l2 = new Line(t1, t2);
 
         System.out.println(t1);
         System.out.println(t2);
-        System.out.println(t3);
 
-        System.out.println(t1==t2);
-        System.out.println(t1==t3);
+        System.out.println(l1);
+        System.out.println(l2);
+
 
     }
 }
